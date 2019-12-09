cac730b4d4f6b6afdb9481236e846d4aa21d1207
ozubergz <jmin499@gmail.com>
2019-12-08 22:33:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
+const array = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  const found = record.find(obj =>  obj.result === "W");
+  return found ? found.year : undefined;
 }
+
+superbowlWin(array)
